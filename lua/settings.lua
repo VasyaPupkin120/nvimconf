95efe1ca3d52450d15442c74487ec3d5b940e7fc
VasyaPupkin120 <VPupkinza@yandex.ru>
2022-10-04 17:58:59 +0700

    добавил код для поддержки dm-langserver. Работоспособно, но требует разобраться с дублированием кода в настройках маппинга для lsp-серверов

--- lua/settings.lua
+++ lua/settings.lua
@@ -180,3 +180,55 @@ cmd [[lua require('Comment').setup()]]
 cmd [[lua require("bufferline").setup()]]
 -- чтобы работал проводник файлов по F6
 cmd [[lua require("nvim-tree").setup()]]
+
+
+-- настройки для dm-langserver
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+-- автоопределение типа .dm
+vim.cmd [[ autocmd BufNewFile,BufRead *.dm,*.dme set filetype=dm ]]
+
+-- запуск dm-langserver
+require('lspconfig')['dm_langserver'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+-- отключение замены табов на пробелы для dm-файлов
+cmd [[autocmd FileType dm setlocal noexpandtab]]
